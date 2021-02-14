77bd2639e8d91c50cacec130ddb4f3c3cce5db86
ankitsharma1995 <62223340+ankitsharma1995@users.noreply.github.com>
2021-02-14 17:04:58 -0500

    Add files via upload

---
+++ refund-policy.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+
+<html>
+
+
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+    <title>International Courier Service</title>
+	<meta name="description" content=""/>
+	<meta name="keywords" content="" />
+	<meta name="author" content="">	
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+ 
+	<link href="deprixa_components/content/cssefe4.css" rel="stylesheet"/>  
+    <link href="deprixa_components/styles/track-order.css" rel="stylesheet" />
+    
+    <!--[if lte IE 9]><link rel="stylesheet" href="/hub/css/ie9.css" /><![endif]-->
+    <!--[if lte IE 8]><script src="/hub/js/html5shiv.js"></script><link rel="stylesheet" href="/hub/css/ie8.css" /><![endif]-->
+    <!--[if lte IE 7]><link rel="stylesheet" href="/hub/css/ie7.css" /><![endif]-->
+	
+	<link rel="icon" type="image/png" href="../favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="../favicon-16x16.png" sizes="16x16" />
+
+</head>
+   <!-- Menu -->
+<?php include_once "menu.php"; ?>
+    <!-- /Menu -->
+
+<main class="slide">
+
+<div class="fw col-lg-12">
+<section class="cookies">
+<br><br>
+<h1>Refund Policy</h1>
+<br><br>
+<ol>
+<li><span></span>You may qualify for a postage refund if : You are the sender of the parcel and you have a copy of the mailing receipt or You used one of our On-Time Delivery Guarantee services.</li>
+<li><span></span>Postage refunds for parcels delivered within Canada : You sent the parcel and you have a copy of the mailing receipt. (If you are the parcel’s recipient, ask the sender to start the refund claim.)The parcel was shipped using Priority™ or Xpresspost™ or Expedited Parcel™ (commercial only) service.The parcel arrived after the guaranteed delivery date. Use Track to ensure that date has passed.You submit your claim within 30 business days of the guaranteed delivery date.</li>
+<li><span></span>Postage refunds for parcels delivered to the U.S. or international destinations : You sent the parcel and you have a copy of the mailing receipt. (If you are the parcel’s recipient, ask the sender to start the refund claim.) The parcel was shipped using Priority™ Worldwide, Xpresspost™ USA or Xpresspost™ International service. Your parcel was delivered later than the guaranteed time for its destination. Your parcel didn’t have restrictions that may have caused delays in customs.</li>
+<li><span></span>In case of COD order we shall be attaching the invoice with the package mentioning the generic name of the product category eg. garments, personal care etc.</li>
+<li><span></span>Delivery delays due to customer unavailability or discrepancies in billing address or failures in providing contact number for delivery. (If the courier has come to the shipping address within seven business days and not been able to get delivered. The courier may or may not contact you before coming - that is something we have no control over) </li>
+<li><span></span> For orders which are placed using Credit Card/Net Banking Payment Option and are under Verification, the guarantee is applicable post the payment has been verified.</li>
+</section>
+</div>
+
+</main>
+
+    <!-- Footer -->
+<?php include_once "footer.php"; ?>
+    <!-- /Footer -->
+	</div>
+
+	<script src="deprixa_components/hub/scripts/services/services.js"></script>
+	<script src="deprixa_components/hub/scripts/dSwiper.min.js"></script>	
+	<script type='text/javascript' src='deprixa_components/scripts/MPD/integration.js'></script>
+	<script type='text/javascript' src='deprixa_components/scripts/MPD/Common/ga-events.js'></script>
+	<script src="deprixa_components/bundles/jquery"></script>
+   <script src="deprixa_components/bundles/bootstrap"></script>
+    <script src="deprixa_components/bundles/modernizr"></script>
+    <script src="deprixa_components/scripts/CookieManager.js"></script>
+    <script src="deprixa_components/scripts/MPD/Common/ga-events.js"></script>   
+    <script src="deprixa_components/bundles/jqueryval"></script>
+    <script src="deprixa_components/scripts/tracking.js"></script>
+    <script src="deprixa_components/scripts/placeholder-shim.js"></script>
+    <script src="deprixa_components/scripts/trimFields.js"></script>
+
+
+</body>
+
+</html>
