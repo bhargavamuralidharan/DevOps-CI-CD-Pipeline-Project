a1726eff74dd20fff8a7a29ec7dbcf097e7cdff7
aennisjr <aggreyennis@outlook.com>
2021-02-28 23:32:36 -0500

    Update SQL schema

--- devops-ci.sql
+++ devops-ci.sql
@@ -1,86 +1,237 @@
-CREATE TABLE `user` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `username` varchar(255),
-  `title` varchar(5),
-  `first_name` varchar(255),
-  `last_name` varchar(255),
-  `email` varchar(255),
-  `email_verified` int,
-  `password` varchar(255),
-  `phone` int,
-  `company` varchar(255),
-  `prime_member` int,
-  `email_notifications` int,
-  `deals_notifications` int,
-  `sms_notifications` int,
-  `registered` timestamp,
-  `access` int,
-  `hash` varchar(255)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 01, 2021 at 05:15 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.3.26
 
-CREATE TABLE `message` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `user_id` int,
-  `subject` varchar(255),
-  `content` varchar(255),
-  `sent` timestamp,
-  `sender_del` int,
-  `recipient_del` int
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `address` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `user_id` int,
-  `deliver_to` varchar(255),
-  `address` varchar(255),
-  `city` varchar(255),
-  `province` varchar(255),
-  `zip_code` varchar(255),
-  `country` varchar(255)
-);
 
-CREATE TABLE `delivery` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `address_id` int,
-  `user_id` int,
-  `delivery_date` timestamp
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `delivery_status` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `delivery_id` int,
-  `status` int,
-  `d_date` timestamp
-);
+--
+-- Database: `devops-ci`
+--
 
-CREATE TABLE `notification` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `user_id` int,
-  `type` int,
-  `method` int,
-  `content` varchar(255),
-  `read` int,
-  `sent` timestamp
-);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account_recovery`
+--
 
 CREATE TABLE `account_recovery` (
-  `id` int,
-  `email` varchar(255),
-  `requests` int,
-  `slug` varchar(255),
-  `request_date` timestamp
-);
+  `id` int(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `requests` int(11) DEFAULT NULL,
+  `request_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `message` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `address` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+--
+-- Table structure for table `address`
+--
 
-ALTER TABLE `address` ADD FOREIGN KEY (`id`) REFERENCES `delivery` (`address_id`);
+CREATE TABLE `address` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `deliver_to` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `province` varchar(255) DEFAULT NULL,
+  `zip_code` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `delivery_status` ADD FOREIGN KEY (`delivery_id`) REFERENCES `delivery` (`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `user` ADD FOREIGN KEY (`id`) REFERENCES `delivery` (`user_id`);
+--
+-- Table structure for table `delivery`
+--
 
-ALTER TABLE `account_recovery` ADD FOREIGN KEY (`email`) REFERENCES `user` (`email`);
+CREATE TABLE `delivery` (
+  `id` int(11) NOT NULL,
+  `address_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `delivery_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `notification` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `delivery_status`
+--
+
+CREATE TABLE `delivery_status` (
+  `id` int(11) NOT NULL,
+  `delivery_id` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `d_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `sent` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `sender_del` int(11) DEFAULT NULL,
+  `recipient_del` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE `notification` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `method` int(11) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `read` int(11) DEFAULT NULL,
+  `sent` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `title` varchar(5) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `email_verified` int(11) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `company` varchar(255) DEFAULT NULL,
+  `prime_member` int(11) DEFAULT NULL,
+  `email_notifications` int(11) DEFAULT NULL,
+  `deals_notifications` int(11) DEFAULT NULL,
+  `sms_notifications` int(11) DEFAULT NULL,
+  `registered` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `delivery`
+--
+ALTER TABLE `delivery`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `delivery_status`
+--
+ALTER TABLE `delivery_status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `address`
+--
+ALTER TABLE `address`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `delivery`
+--
+ALTER TABLE `delivery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `delivery_status`
+--
+ALTER TABLE `delivery_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `address_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
