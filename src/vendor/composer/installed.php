d296df7f012002c27d70f92cbb402f1362d665ee
aennisjr <aggreyennis@outlook.com>
2021-03-04 01:23:03 -0500

    Add Twilio API integration

---
+++ src/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b5217c1dab5d2b72d294df878256e111db053c6e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b5217c1dab5d2b72d294df878256e111db053c6e',
+    ),
+    'twilio/sdk' => 
+    array (
+      'pretty_version' => '5.42.2',
+      'version' => '5.42.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0cfcb871b18a9c427dd9e8f0ed7458d43009b48a',
+    ),
+  ),
+);
