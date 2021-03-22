8cde5b33fb93576cab54bcdf4eb5130e43b6fe00
root <root@bharath.us-central1-a.c.clear-variety-306801.internal>
2021-03-22 19:21:39 +0000

    commiting session data

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://35.222.37.197:8001/';
+$config['base_url'] = 'http://35.226.36.48:8001/';
 /*
 |--------------------------------------------------------------------------
 | Index File
